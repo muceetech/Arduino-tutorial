e4ec303eff21c6c1cfd44a3bcaab0134ab737058
muceetech <65214908+muceetech@users.noreply.github.com>
2020-05-15 22:26:36 +0530

    Update temperaturesensortmp36.ino

--- temperaturesensortmp36.ino
+++ temperaturesensortmp36.ino
@@ -13,7 +13,8 @@ void loop()
   sensorval = analogRead(A0);     //read the analog sensor and store it
   temp = (sensorval *5.0)/1023.0; // convert the analog value to voltage
   temp = temp - 0.5;              // subtract the offset voltage = 0.5V for TMP36 sensor to get temperature         
-   
+  int val = temp * 100;   // Convert voltage to temperature
+ 
   Serial.print("Temperature in Celcius: ");
   Serial.println(temp);
 }
