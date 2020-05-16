531326aa07b9481299483945625a7a5961105ccf
muceetech <65214908+muceetech@users.noreply.github.com>
2020-05-16 20:45:46 +0530

    Update servomotor.ino

--- servomotor.ino
+++ servomotor.ino
@@ -1,7 +1,9 @@
+#include <Servo.h>
+
 Servo myservo;  // create servo object to control a servo
 
-int pos = 0;    // variable to store the servo position
-
+int angle = 0;    // variable to store the servo position
+int analogval; 
 void setup() 
 {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
@@ -9,7 +11,9 @@ void setup()
 
 void loop() 
 {
-    myservo.write(0);  
-    delay(15);  
-  }
+  val = analogRead(A0); // Ao is connected to potentiometer
+  angle = map(val,0,1023,0,255);
+  myservo.write(angle);  
+  delay(15);  
+  
 }
